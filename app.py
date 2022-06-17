d53ebcf9a2a2e12f9065be0c87f5a013dc27e6e5
f3brucotech <107501524+f3brucotech@users.noreply.github.com>
2022-06-17 10:28:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -230,12 +230,13 @@ async def command(ack, body, respond, client, logger):
             "type": "input",
             "block_id": "the_ao",
             "element": {
-                "type": "plain_text_input",
-                "action_id": "the_ao",
+                "type": "channels_select",
                 "placeholder": {
                     "type": "plain_text",
-                    "text": "Enter the AO Name"
-                }
+                    "text": "Select the AO",
+                    "emoji": True
+                },
+                "action_id": "channels_select-action"
             },
             "label": {
                 "type": "plain_text",
@@ -417,7 +418,7 @@ async def view_submission(ack, body, logger, client):
     result = body["view"]["state"]["values"]
     title = result["title"]["title"]["value"]
     date = result["date"]["datepicker-action"]["selected_date"]
-    the_ao = result["the_ao"]["the_ao"]["value"]
+    the_ao = result["the_ao"]["channels_select-action"]["selected_channel"]
     the_q = result["the_q"]["users_select-action"]["selected_user"]
     pax = result["the_pax"]["multi_users_select-action"]["selected_users"]
     fngs = result["fngs"]["fng-action"]["value"]

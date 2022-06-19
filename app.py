c1739f1d17fdfbc1bc7f5bf52d5febc480d32ba1
f3brucotech <107501524+f3brucotech@users.noreply.github.com>
2022-06-18 21:34:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -519,7 +519,7 @@ async def view_submission(ack, body, logger, client):
     title = result["title"]["title"]["value"]
     date = result["date"]["datepicker-action"]["selected_date"]
     the_ao = result["the_ao"]["channels_select-action"]["selected_channel"]
-    the_wrkout = result["the_wrkout"]["static_select-action"]["selected_option"]["value"]
+    the_wrkout = result["the_wrkout"]["static_select-action"]["value"]
     the_q = result["the_q"]["users_select-action"]["selected_user"]
     pax = result["the_pax"]["multi_users_select-action"]["selected_users"]
     fngs = result["fngs"]["fng-action"]["value"]
